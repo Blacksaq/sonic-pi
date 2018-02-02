0551334bdec19e987c091124fbbfa153cb3835d9
Blacksaq <ahtls24@gmail.com>
2018-02-02 14:46:57 -0800

    Create Ode_2_joY.rb

---
+++ Ode_2_joY.rb
@@ -0,0 +1,134 @@
+use_synth :piano
+use_bpm 120
+1.times do
+  play :e4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :e4, sustain: 2
+  sleep 1.5
+  play :d4, sustain: 1
+  sleep 0.5
+  play :d4, sustain: 1.5
+  sleep 1.5
+end
+1.times do
+  play :e4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1.5
+  play :c4, sustain: 1
+  sleep 0.5
+  play :c4, sustain: 1
+  sleep 1.5
+end
+
+1.times do
+  play :d4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 0.5
+  play :f4, sustain: 1
+  sleep 0.5
+  play :e4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 0.5
+  play :f4, sustain: 1
+  sleep 0.5
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1.5
+end
+
+1.times do
+  play :e4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :g4, sustain: 1
+  sleep 1
+  play :f4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :c4, sustain: 1
+  sleep 1
+  play :d4, sustain: 1
+  sleep 1
+  play :e4, sustain: 1
+  sleep 1
+  play :d4, sustain: 2
+  sleep 1
+  play :c4, sustain: 2
+  sleep 1
+  play :c4, sustain: 1
+  sleep 0.5
+end
