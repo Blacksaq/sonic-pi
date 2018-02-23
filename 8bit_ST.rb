6d5a89add3a45a5f85ced86ad296c9d502290300
Blacksaq <ahtls24@gmail.com>
2018-02-23 14:06:43 -0800

    Update 8bit_ST.rb

--- 8bit_ST.rb
+++ 8bit_ST.rb
@@ -1,64 +1,31 @@
+use_bpm 160
 use_synth :piano
-use_bpm 240
-with_fx :reverb do
-  with_fx :slicer do
-    3.times do
-      1.times do
-        play :c4
-        sleep 1
-        play :e4
-        sleep 1
-        play :g4
-        sleep 1
-        play :b4
-        sleep 1
-        play :c5
-        sleep 1
-        play :b4
-        sleep 1
-        play :g4
-        sleep 1
-        play :e4
-        sleep 1
-        play :c4
-        sleep 1
-        play :e4
-        sleep 1
-        play :g4
-        sleep 1
-        play :b4
-        sleep 1
-        play :c5
-        sleep 1
-      end
-      1.times do
-        play :b4
-        sleep 1
-        play :g4
-        sleep 1
-        play :e4
-        sleep 1
-        play :c4
-        sleep 1
-        play :e4
-        sleep 1
-        play :g4
-        sleep 1
-        play :b4
-        sleep 1
-        play :c5
-        sleep 1
-        play :b4
-        sleep 1
-        play :g4
-        sleep 1
-        play :e4
-        sleep 1
-        play :c4
-        sleep 1
-        play :e4
-        sleep 1
-      end
-    end
-  end
+notes = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+
+=begin
+live_loop :strategy1 do
+  # assign a value to 'a' using the .choose method from the Randomisation reading on Tuesday
+  a = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+  print a
+  play a [dice(7)]
+  sleep 0.5
+end
+
+
+
+live_loop :strategy2 do
+  # assign a value to 'b' that will be used as the index value
+  b = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+  print b
+  play b.choose
+  sleep 0.5
+end
+=end
+
+live_loop :strategy3 do
+  # assign a value to 'c' that will be used as the index value
+  c = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+  print c
+  play c [rrand_i(0,7)]
+  sleep 0.5
 end
